1cbbb51bdf9cf9b39b94895c5c99a43581b08f7b
ezz khalaf <ezz.awni@gmail.com>
2025-07-01 19:59:41 +0300

    ...

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
 const express = require("express");
 const bodyParser = require('body-parser')
 const { graphqlHTTP } = require("express-graphql");//a middlware function for express in graphql
-const {buildSchema} = require('graphql')
+const {buildSchema} = require('graphql');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+const Event = require('./model/event')
+
+dotenv.config();
 
 const app = express();
+
+
+const events = []
 
 app.use(bodyParser.json())
 app.use("/api" , graphqlHTTP({
     schema : buildSchema(`
+
+        type Event {
+            _id : ID! 
+            title : String!
+            description : String!
+            price : Float!
+            date : String!
+        }
+
+        input EventInput {
+            title : String!
+            description : String!
+            price : Float!
+            date : String!
+        }
+
         type RootQuery {
-        
+            events : [Event!]!
         }
         
         type RootMutation {
-
+            createEvent(eventInput : EventInput) : Event!
         }
+
         schema {
-                query :RootQuery  ,
+                query :RootQuery  
                 mutation : RootMutation
         }
         `) ,
-    rootValue : {
+    rootValue : { //resolver
+        events : () => {
+            return events
+        },
+        createEvent: (args) => {
+            const eventInput = args.eventInput;
 
+            const event = {
+                _id: Math.random().toString(),
+                title: eventInput.title,
+                description: eventInput.description,
+                price: +eventInput.price,
+                date: eventInput.date
+            };
+
+            events.push(event);
+
+            return event; 
+        },
+        graphiql : true //will explained later  
     }
 }))
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT ,() =>  console.log(`listening to port ${PORT}`) );
+mongoose.connect(process.env.DB_CONNECTION_STRING)
+  .then(() => {
+    console.log("--DB connected");
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
